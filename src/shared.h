1ba3c812549abb440f906c1c7fdbcbff9c30c5d9
Mika Schipper <mschippe@student.codam.nl>
2025-02-07 03:19:18 +0100

    removed one more include

--- src/shared.h
+++ src/shared.h
@@ -6,14 +6,13 @@
 /*   By: mika <mika@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/02/07 01:53:50 by mika          #+#    #+#                 */
-/*   Updated: 2025/02/07 03:14:14 by mika          ########   odam.nl         */
+/*   Updated: 2025/02/07 03:17:44 by mika          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SHARED_H
 # define SHARED_H
 # include <signal.h>
-# include <stdlib.h>
 # include <unistd.h>
 # include "../libft/libft.h"
 
